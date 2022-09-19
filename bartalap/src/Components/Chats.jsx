7a209ffdfd73f854e4a0b1240319f1aa3ce7471a
Pritam Kumar Pradhan <pradhanpritam14@gmail.com>
2022-09-19 22:43:11 +0530

    Day 4 commit

--- bartalap/src/Components/Chats.jsx
+++ bartalap/src/Components/Chats.jsx
@@ -6,10 +6,32 @@ const Chats = () => {
      <div className="userchat">
         <img src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
         <div className="userchatinfo">
-          <span>Pritam</span>
+         <span>Sweety</span>
+         {/* <p>Hello</p> */}
         </div>
      </div>
-   </div>
+
+     <div className="userchat">
+        <img src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className="userchatinfo">
+          <span>Jyoti</span>
+        </div>
+     </div>
+
+     <div className="userchat">
+        <img src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className="userchatinfo">
+          <span>Chiro</span>
+        </div>
+     </div>
+
+     <div className="userchat">
+        <img src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className="userchatinfo">
+          <span className='pk'>Somesh</span>
+        </div>
+     </div>
+   </div> 
   )
 }
 
--- bartalap/src/Components/Message.jsx
+++ bartalap/src/Components/Message.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>
-      
+    <div className="message owner">
+      <div className="messageinfo">
+        <img src="https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <span>Just now</span>
+      </div>
+      <div className="messagecontent">
+        <p>Hello</p>
+        <img src="https://images.pexels.com/photos/324672/pexels-photo-324672.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
+      </div>
     </div>
   )
 }
